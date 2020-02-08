40871e331332cac198166c7da854c111075d58c0
Ryan M <ryanpmcn@gmail.com>
2020-02-08 09:26:38 -0500

    Added type and Parameter information. Parameters are Innerclass of DatabaseProgram.

--- DbEnumerator/DbEnumeratorLib/DatabaseProgram.cs
+++ DbEnumerator/DbEnumeratorLib/DatabaseProgram.cs
@@ -1,10 +1,20 @@
-﻿namespace DbEnumerator
+﻿using System.Collections.Generic;
+
+namespace DbEnumerator
 {
+    public enum ProgramType { View, ScalarValueFunction, TableValueFunction, StoredProcedure }
     public class DatabaseProgram
     {
+        public class Parameter
+        {
+            public string Name { get; set; }
+            public object Value { get; set; }
+        }
+        public ProgramType Type {get;set;}
         public string Database { get; set; }
         public string Schema { get; set; }
         public string Name { get; set; }
+        public List<Parameter> Parameters { get; set; }
         public override string ToString()
         {
             return $"{Database}.{Schema}.{Name}";
