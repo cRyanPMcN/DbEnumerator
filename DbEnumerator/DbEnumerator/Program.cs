2357b05a48ac67509415a80399288c0a7c0b623a
Ryan M <ryanpmcn@gmail.com>
2020-02-14 12:04:07 -0500

    Made CLI compatible with the library.

--- DbEnumerator/DbEnumerator/Program.cs
+++ DbEnumerator/DbEnumerator/Program.cs
@@ -53,22 +53,46 @@ namespace DbEnuemratorCLI
 
                     using SqlConnection dbConn = new SqlConnection(sqlConnBuilder.ConnectionString);
                     Enumerator dbEnumerator = new Enumerator(dbConn);
-                    List<DatabaseProgram> programs = dbEnumerator.GetDatabasePrograms();
-                    //Enumerator.SetXMLPlanOn(dbConn);
-
-                    //Enumerator.SetXMLPlanOff(dbConn);
-                    foreach (DatabaseProgram program in programs)
+                    IEnumerable<IDatabaseProgram> programs = dbEnumerator.GetDatabasePrograms();
+                    foreach (IDatabaseProgram p in programs)
                     {
-                        Console.WriteLine(program.ToString());
+                        dbEnumerator.GetProgramParameters(p);
+                        //Console.WriteLine(p.QueryString);
                     }
+                    dbEnumerator.SetXMLPlanOn();
+                    XmlDocument doc = new XmlDocument();
+                    string qryplan = dbEnumerator.GetQueryPlan(programs.Skip(2).First());
+                    //Console.WriteLine(qryplan);
+                    StringReader strReader = new StringReader(qryplan);//dbEnumerator.GetQueryPlan(programs.Skip(2).First()));
+                    doc.Load(strReader);
+
+                    IEnumerable<Database> queryData = dbEnumerator.DatabaseCollectionFactory(doc);
+                    foreach (Database database in queryData)
+                    {
+                        foreach (Schema schema in database.Schemas)
+                        {
+                            foreach (DataTable table in schema.Tables)
+                            {
+                                foreach (string column in table.Columns)
+                                {
+                                    Console.WriteLine($"{database.Name}.{schema.Name}.{table.Name}.{column}");
+                                }
+                            }
+                        }
+                    }
+                    dbEnumerator.SetXMLPlanOff();
+                    //foreach (IDatabaseProgram program in programs)
+                    //{
+                    //    Console.WriteLine(program.ToString());
+                    //}
                 }
                 else if (args.Length == 1)
                 {
                     using SqlConnection dbConn = new SqlConnection(args[0]);
                     Enumerator dbEnumerator = new Enumerator(dbConn);
-                    List<DatabaseProgram> dbInfo = dbEnumerator.GetDatabasePrograms();
-                    List<DatabaseProgram> programs = dbEnumerator.GetDatabasePrograms();
-                    foreach (DatabaseProgram program in programs)
+                    IEnumerable<IDatabaseProgram> dbInfo = dbEnumerator.GetDatabasePrograms();
+                    IEnumerable<IDatabaseProgram> programs = dbEnumerator.GetDatabasePrograms();
+                    foreach (IDatabaseProgram program in programs)
                     {
                         Console.WriteLine(program.ToString());
                     }
