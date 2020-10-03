8eb459ea74677c17a565c10664062ecca63b8e3a
Ryan M <ryanpmcn@gmail.com>
2020-10-02 23:55:36 -0400

    Code Cleanup: - Removed Print_XML - Changed nested if in Main to be a switch - Removed unneccesary ToString calls

--- DbEnumerator/DbEnumerator/Program.cs
+++ DbEnumerator/DbEnumerator/Program.cs
@@ -1,52 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.Xml.XPath;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
-using System.IO;
 using System.Data.SqlClient;
-using System.Data.Sql;
-using DbEnumerator;
-using System.Diagnostics;
+using DbEnumeratorLib;
 
 namespace DbEnuemratorCLI
 {
     partial class Program
     {
-        static void Print_XML(XmlNode doc, Int32 depth = 0)
+        static void PrintProgramUsage()
         {
-            Console.Write(new string('\t', depth));
-            Console.WriteLine(doc.OuterXml);
-            foreach (XmlNode child in doc.ChildNodes)
-            {
-                Print_XML(child, ++depth);
-            }
+            Console.WriteLine("Usage: DbEnumerator.exe ([Connectionstring]) | ([Data Source] [UserId] [Password] [Initial Catalog])");
         }
 
-        static void PrintProgramArguments()
-        {
-            Console.WriteLine("DbEnumerator.exe ([Connectionstring]|[Data Source] [UserId] [Password] [Initial Catalog]");
-        }
-
-        static bool RunEnumerator(SqlConnection dbConn)
+        static void RunEnumerator(SqlConnection dbConn)
         {
             Enumerator dbEnumerator = new Enumerator(dbConn);
-            ICollection<IDatabaseProgram> programs = dbEnumerator.GetDatabasePrograms();
-            // Scope to separate programCount from rest of function
+            ICollection<IProgram> programs = dbEnumerator.GetDatabasePrograms();
+            int programCount = 1;
+            foreach (IProgram p in programs)
             {
-                int programCount = 0;
-                foreach (IDatabaseProgram p in programs)
-                {
-                    dbEnumerator.GetProgramParameters(p);
-                    Console.WriteLine($"[{programCount++}]: {p.ToString()}");
-                }
+                Console.WriteLine($"[{programCount}]: {p}");
+                ++programCount;
             }
+
             int selectedIndex = -1;
-            while (selectedIndex < 0 || selectedIndex > programs.Count-1) {
-                Console.WriteLine($"Please enter an integer between 0 and {programs.Count-1}.");
+            while (selectedIndex < 1 || selectedIndex > programs.Count) 
+            {
+                Console.WriteLine($"Please enter an integer between 1 and {programs.Count}.");
                 try
                 {
                     selectedIndex = int.Parse(Console.ReadLine());
@@ -65,19 +47,17 @@ namespace DbEnuemratorCLI
                 }
             }
 
-            IDatabaseProgram selectedProgram = programs.ElementAt(selectedIndex);
+            IProgram selectedProgram = programs.ElementAt(selectedIndex - 1);
             dbEnumerator.SetXMLPlanOn();
-            XmlDocument doc = new XmlDocument();
-            string qryplan = dbEnumerator.GetQueryPlan(selectedProgram);
-            StringReader strReader = new StringReader(qryplan);
-            doc.Load(strReader);
+            XmlDocument queryPlan = dbEnumerator.GetQueryPlan(selectedProgram);
+            dbEnumerator.SetXMLPlanOff();
 
-            ICollection<Database> databases = dbEnumerator.DatabaseCollectionFactory(doc);
+            ICollection<Database> databases = dbEnumerator.DatabaseCollectionFactory(queryPlan);
             foreach (Database database in databases)
             {
                 foreach (Schema schema in database.Schemas)
                 {
-                    foreach (DataTable table in schema.Tables)
+                    foreach (Table table in schema.Tables)
                     {
                         foreach (string column in table.Columns)
                         {
@@ -86,25 +66,30 @@ namespace DbEnuemratorCLI
                     }
                 }
             }
+
             if (databases.Count == 0)
             {
-                Console.WriteLine($"Query plan for {selectedProgram.ToString()} does not contain a reference to a database column");
+                Console.WriteLine($"Query plan for {selectedProgram} does not contain a reference to a database column");
             }
-            dbEnumerator.SetXMLPlanOff();
-            return true;
         }
 
         static void Main(string[] args)
         {
             try
             {
-
                 Console.WriteLine("Welcome to DbEnumerator");
                 Console.WriteLine("Checking program Args");
-                if (args.Length > 0)
+                switch (args.Length)
                 {
-                    if (args.Length == 4)
-                    {
+                    case 0:
+                        Console.WriteLine("No arguments found, please enter a MSSQL-Server Connection string to continue:");
+                        string connstring = Console.ReadLine();
+                        RunEnumerator(new SqlConnection(connstring));
+                        break;
+                    case 1:
+                        RunEnumerator(new SqlConnection(args[0]));
+                        break;
+                    case 4:
                         SqlConnectionStringBuilder sqlConnBuilder = new SqlConnectionStringBuilder()
                         {
                             DataSource = args[0],
@@ -112,37 +97,17 @@ namespace DbEnuemratorCLI
                             Password = args[2],
                             InitialCatalog = args[3],
                         };
+
                         RunEnumerator(new SqlConnection(sqlConnBuilder.ConnectionString));
-                    }
-                    else if (args.Length == 1)
-                    {
-                        using SqlConnection dbConn = new SqlConnection(args[0]);
-                        RunEnumerator(new SqlConnection(args[0]));
-                    }
-                    else
-                    {
-                        PrintProgramArguments();
-                        return;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("No arguments found, please enter a MSSQL-Server Connection string to continue:");
-                    string connstring = Console.ReadLine();
-                    using SqlConnection dbConn = new SqlConnection(connstring);
-                    RunEnumerator(dbConn);
+                        break;
+                    default:
+                        PrintProgramUsage();
+                        break;
                 }
             }
             catch (Exception ex)
             {
-                // Currently dropping StackTrace
                 Console.WriteLine($"{ex.GetType().FullName}: {ex.Message}");
-                Exception innerException = ex.InnerException;
-                while (innerException != null)
-                {
-                    Console.WriteLine($"{ex.GetType().FullName}: {ex.Message}");
-                    innerException = ex.InnerException;
-                }
             }
         }
     }
